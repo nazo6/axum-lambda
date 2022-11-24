f74ee31173b1eb800fd113e33b01d3ff3f0b534a
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-11-24 18:54:53 +0900

    Update

--- main.tf
+++ main.tf
@@ -19,15 +19,36 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+resource "aws_iam_role" "iam_axum_lambda_test" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "this" {
   function_name = "axum_lambda_test"
   description   = "Rust lambda sample."
+  role          = aws_iam_role.iam_axum_lambda_test.arn
 
   filename         = data.archive_file.zip.output_path
   source_code_hash = data.archive_file.zip.output_base64sha256
 
-  runtime      = "provided.al2"
-  architecture = "arm64"
+  runtime       = "provided.al2"
+  architectures = "arm64"
 }
 
 resource "null_resource" "rust_build" {
